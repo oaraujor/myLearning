9ec72a353a9435fa2943d476222354752665569a
Octavio Araujo <oaraujor_@hotmail.com>
2025-04-21 18:23:11 -0600

    creacion de estructurad e alumno y creacion de menu principal

--- main.c
+++ main.c
@@ -2,5 +2,35 @@
 
 void main()
 {
-    printf("inicio");
+    int opcion;
+    do
+    {
+
+        printf("\tMENU]\n");
+        printf("1) Alta de Pacientes\n");
+        printf("2) Busqueda o edicion de paciente");
+        printf("3) Listado de pacientes");
+        printf("4) Baja de paciente");
+        printf("5) Salir");
+        break;
+
+        switch(opcion)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                printf("Opcion no valida");
+                break;
+
+        }
+    }while(opcion != 5);
+
 }
