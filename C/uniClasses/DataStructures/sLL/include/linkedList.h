b3ffe228e728ffd9a27a5f4b78872475e0c9a0fb
Octavio Araujo Rosales <oaraujor_@hotmail.com>
2025-08-20 11:13:50 -0600

    add/ added insert in front and end of the linked list

--- C/uniClasses/DataStructures/sLL/include/linkedList.h
+++ C/uniClasses/DataStructures/sLL/include/linkedList.h
@@ -1,25 +1,118 @@
 #ifndef sLL
 #define sLL
+
+#define ERROR_MSG printf("Error with malloc\n");
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct LLNode {
+typedef struct node {
 	int value;
-	struct LLNode *next;
-} LLNode;
+	struct node *next;
+} NODE_LL;
 
-void initList(LLNode* );
-void updateValueNode(LLNode* );
+void apndBegOfList(NODE_LL**, int*);
+void apndEndOfList(NODE_LL**, int*);
+void printList(NODE_LL** );
+void killList(NODE_LL **);
 
-void initList(LLNode* node) { //init the first node of the linkedList
-	node->next = NULL;
+
+void apndBegOfList(NODE_LL** list, int *value) {
+	/*
+	Function to append a value to the linked list.
+	[Inserts here]->[]->[]->NULL
+	
+	args:
+		NODE_LL** list - head of the list
+		int* value - address of the int variable to be stored;
+	*/
+	NODE_LL* newNODE_LL;
+
+	newNODE_LL = malloc(sizeof(NODE_LL));
+	if(newNODE_LL != NULL) {
+		newNODE_LL->value = *value;
+		newNODE_LL->next = NULL;
+		newNODE_LL->next = *list;
+		*list = newNODE_LL;
+	}
+	else {
+		ERROR_MSG
+	}
 }
 
-void updateValueNode(LLNode* node) { //simple function to update node value
-	int valueIn;
-	printf("insert int value to store in node\n");
-	scanf("%d", &valueIn);
-	node->value = valueIn;
+void apndEndOfList(NODE_LL **list , int * value)
+{
+	/*
+	Function to append a value to the linked list.
+	[]->[]->[Inserts Here]->NULL
+
+	args:
+		NODE_LL **list - head of the list
+		int *value - address of the int variable to be stored
+	*/
+
+	NODE_LL *newNODE, *prevNODE, *currNODE;
+
+	newNODE = malloc(sizeof(NODE_LL));
+	if(newNODE != NULL) {
+		newNODE->value = *value;
+		newNODE->next = NULL;
+		prevNODE = NULL;
+		currNODE = *list;
+		while(currNODE != NULL) {
+			prevNODE = currNODE;
+			currNODE = currNODE->next;
+		}
+		if (prevNODE == NULL) {
+			*list = newNODE;
+		}
+		else {
+			prevNODE->next = newNODE;
+		}
+	}
+	else {
+		ERROR_MSG
+	}
+
 }
 
+void printList(NODE_LL** list) {
+	/*
+	Prints the Linked List onto stdin.
+	Format: []->[]->[]->NULL
+	
+	args: NODE **list - head of the list
+	returns: Nothing
+	*/
+
+	NODE_LL *curr;
+	
+	curr = *list;
+
+	while(curr != NULL) {
+		printf("[%d]-->", curr->value);
+		curr = curr->next;
+	}
+	printf("NULL\n");
+}
+
+void killList(NODE_LL **list) {
+	/*
+	Kills (Eliminates) all nodes from the Linked List.
+
+	args: NODE **list - head of the list
+	returns: Nothing
+	*/
+	NODE_LL *curr = NULL, *temp = NULL;
+	curr = *list;
+
+	while(curr != NULL) {
+		temp = curr->next;
+		free(curr);
+		curr = temp;
+	}
+
+}
 
 #endif
--- C/uniClasses/DataStructures/sLL/src/main.c
+++ C/uniClasses/DataStructures/sLL/src/main.c
@@ -1,17 +1,80 @@
 #include "linkedList.h"
 
+bool contRunning(const char *);
+void showOptions();
+
+
 int main()
 {
-	LLNode head;
-	initList(&head);
-	
-	if(head.next == NULL) {
-		printf("INIT HEAD OK\n");
-		updateValueNode(&head);
-		printf("%d is the value of the first node\n", head.value);
-	}
-	else
-		printf("Something wrong\n");
+	NODE_LL* listNumbers;
+	listNumbers = NULL; //empty list
+	int option;
+	int value;
+	bool contMenu, contAppend;
 
+	contMenu = true;
+	while(contMenu) {
+		system("clear");
+		printList(&listNumbers);
+		showOptions();
+		scanf("%d", &option);
+		switch(option) {
+			case 1:
+				do {
+					printf("Enter the int value to append to the beggining of the list: ");
+					scanf("%d", &value);
+					apndBegOfList(&listNumbers, &value);
+					contAppend = contRunning("Append more numbers?\n");
+				}while(contAppend);
+				break;
+
+			case 2:
+				do {
+					system("clear");
+					printf("Enter the int value to append to the end list: ");
+					scanf("%d", &value);
+					apndEndOfList(&listNumbers, &value);
+					contAppend = contRunning("Append more numbers?\n");
+				}while(contAppend);
+				break;
+
+			case 3:
+				contMenu = false;
+				killList(&listNumbers);
+				break;
+
+			default:
+				printf("Unknown Selection!\n");
+				break;
+		}
+
+	}
 	return 0;
+}
+
+void showOptions() {
+	printf("Linked List of Integer Numbers\n");
+	printf("\t1) Append from Front [Here]->[#]->[#]->NULL\n");
+	printf("\t2) Append from End [#]->[#]->[Here]->NULL\n");
+	printf("\t3) Exit\n");
+	printf("\t>>");
+}
+
+bool contRunning(const char *message) {
+	int selection;
+	do {
+		printf("%s", message);
+		printf("|1 - Yes | 0 - NO|\n");
+		printf(">>> ");
+		scanf("%d", &selection);
+		if(selection < 0 || selection > 1){
+			printf("Invalid option\n");
+		}
+	}while(selection < 0 || selection > 1);
+	if (selection == 1) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
